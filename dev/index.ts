32b86e28e2134f19b6f60392f636ae0aed9cd9de
jimao <jimao@jimaodeMacBook-Pro.local>
2023-11-08 15:05:43 +0800

    提交代码

--- dev/index.ts
+++ dev/index.ts
@@ -1,9 +1,22 @@
-import { NFT1, Token1, X } from '../typechain-types';
+import { NFT1, NFT2, NFT3, Token1, Token2, Token3, X } from '../typechain-types';
 import { deployContract, init, meta } from './utils';
 
 async function main() {
   await meta();
+
+  const nft1 = await deployContract<NFT1>('NFT1');
+  console.log('NFT1:', await nft1.getAddress());
+  const nft2 = await deployContract<NFT2>('NFT2');
+  console.log('NFT2:', await nft2.getAddress());
+  const nft3 = await deployContract<NFT3>('NFT3');
+  console.log('NFT3:', await nft3.getAddress());
+
   const token1 = await deployContract<Token1>('Token1');
+  console.log('Token1:', await token1.getAddress());
+  const token2 = await deployContract<Token2>('Token2');
+  console.log('Token2:', await token2.getAddress());
+  const token3 = await deployContract<Token3>('Token3');
+  console.log('Token3:', await token3.getAddress());
 }
 
 async function dev() {
