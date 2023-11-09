74f91ca1941db731c9f9942fcc0ac753f068d79c
jimao <jimao@jimaodeMacBook-Pro.local>
2023-11-09 15:53:45 +0800

    提交代码

--- dev/index.ts
+++ dev/index.ts
@@ -6,6 +6,7 @@ async function main() {
   await meta();
   const nft1 = await getContract<NFT1>('NFT1', '0x5FbDB2315678afecb367f032d93F642f64180aa3');
   const token1 = await getContract<Token1>('Token1', '0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512');
+  const exchange = await getContract<Exchange>('Exchange', '0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0');
   console.log(
     await nft1.name(),
     await token1.name(),
