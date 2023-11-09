b689ace5dae23441644dcbc3dee318ae138a41ad
jimao <jimao@jimaodeMacBook-Pro.local>
2023-11-09 14:51:01 +0800

    提交代码

--- dev/index.ts
+++ dev/index.ts
@@ -4,7 +4,12 @@ import { deployContract, getContract, getSigner, init, meta } from './utils';
 
 async function main() {
   await meta();
-  
+  const nft1 = await getContract<NFT1>('NFT1', '0x5FbDB2315678afecb367f032d93F642f64180aa3');
+  const token1 = await getContract<Token1>('Token1', '0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512');
+  console.log(
+    await nft1.name(),
+    await token1.name(),
+  );
 }
 
 async function dev() {
