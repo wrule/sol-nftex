07a0f1303d9dee52810e34f8189169a305dcba94
jimao <jimao@jimaodeMacBook-Pro.local>
2023-11-09 16:37:11 +0800

    提交代码

--- dev/index.ts
+++ dev/index.ts
@@ -7,10 +7,9 @@ async function main() {
   const signer = getSigner();
   const nft1 = await getContract<NFT1>('NFT1', '0x5FbDB2315678afecb367f032d93F642f64180aa3');
   const token1 = await getContract<Token1>('Token1', '0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512');
-  const exchange = await getContract<Exchange>('Exchange', '0x610178dA211FEF7D417bC0e6FeD39F05609AD788');
+  const exchange = await getContract<Exchange>('Exchange', '0xA51c1fc2f0D1a1b8494Ed1FE312d7C3a78Ed91C0');
   // await nft1.mint();
   console.log(await nft1.balanceOf(signer.address));
-
   // console.log(exchange.orderViews);
 }
 
